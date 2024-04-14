de5a3dadbc8c49c58025e6277a2fe108781fe1bf
VanTruong4465 <114282235+VanTruong4465@users.noreply.github.com>
2024-04-14 10:32:41 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Project1 from './Components/project1';
+import Project2 from './Components/project2';
+import Project3 from './Components/Project3';
+import Project4 from './Components/Project4';
+import Project5 from './Components/Project5';
+import Project6 from './Components/Project6';
+import Project7 from './Components/Project7';
+import Project8 from './Components/Project8';
+import Calculator from './Components/calculator';
+export default function App() {
+  return (
+   //<Project1/>
+   //<Project2/>
+   //<Project3/>
+   //<Project4/>
+   //<Project5/>
+    //<Project6/>
+    //<Project7/>
+    //<Project8/>
+    <Calculator/>
+  );
+}
